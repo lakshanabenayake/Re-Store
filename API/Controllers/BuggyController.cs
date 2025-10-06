b8bb04f360f77ced14a29344bf8fd340770268c2
lakshanabenayake <lakshanabenayake@gmail.com>
2025-10-06 13:18:44 +0530

    inherited controllers from base api conrtoller ,setup new controller to hadnle errors

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("not-found")]
+        public IActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("bad-request")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest("This is a bad request");
+        }
+
+        [HttpGet("unauthorized")]
+        public IActionResult GetUnauthorized()
+        {
+            return Unauthorized();
+        }
+
+        [HttpGet("validate-error")]
+        public IActionResult GetValidationError()
+        {
+            ModelState.AddModelError("Problem1", "This is the first problem");
+            ModelState.AddModelError("Problem2", "This is the second problem");
+            return ValidationProblem();
+        }
+        
+        [HttpGet("server-error")]
+        public IActionResult GetServerError()
+        {
+            throw new Exception("This is a server error");
+        }
+    }
+}
