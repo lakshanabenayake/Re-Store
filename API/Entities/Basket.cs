224a6a02aed65c3c3d3f9ff8eeeb588ec28a95a7
lakshanabenayake <lakshanabenayake@gmail.com>
2025-10-29 02:45:19 +0530

    added new entities

--- API/Entities/Basket.cs
+++ API/Entities/Basket.cs
@@ -7,6 +7,9 @@ public class Basket
     public int Id { get; set; }
     public required string BasketId { get; set; }
     public List<BasketItem> Items { get; set; } = [];
+    public string? ClientSecret { get; set; }
+    public string? PaymentIntentId { get; set; }
+    public AppCoupon? Coupon { get; set; }
 
     public void AddItem(Product product, int quantity)
     {
