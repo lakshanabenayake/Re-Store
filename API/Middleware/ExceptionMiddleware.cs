735d95c01a3e635226a307b70c3cd00ffa27f491
lakshanabenayake <lakshanabenayake@gmail.com>
2025-10-28 09:42:08 +0530

    edited exception middleware

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,47 +1,45 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Text.Json;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
-namespace API.Middleware
+namespace API.Middleware;
+
+public class ExceptionMiddleware(IHostEnvironment env, ILogger<ExceptionMiddleware> logger) 
+    : IMiddleware
 {
-    public class ExceptionMiddleware(ILogger<ExceptionMiddleware> logger, IHostEnvironment env) : IMiddleware
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
-        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        try
         {
-            try
-            {
-                await next(context);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, ex.Message);
-
-                await HandleExceptionAsync(context, ex);
-            }
+            await next(context);
         }
-
-        public async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        catch (Exception ex)
         {
-            logger.LogError(ex, ex.Message);
-            context.Response.ContentType = "application/json";
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-
-            var response = new ProblemDetails
-            {
-                Status = 500,
-                Detail = env.IsDevelopment() ? ex.StackTrace?.ToString() : null,
-                Title = ex.Message
-            };
-
-            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
-            var json = JsonSerializer.Serialize(response, options);
-
-            await context.Response.WriteAsync(json);
+            await HandleException(context, ex);
         }
+    }
+
+    private async Task HandleException(HttpContext context, Exception ex)
+    {
+        logger.LogError(ex, ex.Message);
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var response = new ProblemDetails
+        {
+            Status = 500,
+            Detail = env.IsDevelopment() 
+                ? ex.StackTrace?.ToString()
+                : null,
+            Title = ex.Message
+        };
+
+        var options = new JsonSerializerOptions 
+            {PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+
+        var json = JsonSerializer.Serialize(response, options);
+
+        await context.Response.WriteAsync(json);
     }
 }
